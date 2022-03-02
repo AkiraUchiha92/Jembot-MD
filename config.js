8ff55da893250da8909b80d33e105a9c35f2a3ce
Akira YT Channel <97806782+AkiraUchiha92@users.noreply.github.com>
2022-03-03 03:06:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 
-global.owner = ['6289512545999'] // Put your number here
+global.owner = ['6281327327914'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
